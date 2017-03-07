f2fb89e33ad75ade8e51f50a902cd496b86d9221
Adam Jamka <adam.jamka.1995@gmail.com>
2017-03-07 14:30:49 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,49 +1,148 @@
 #include <iostream>
 #include <fstream>
+
 using namespace std;
 
-class Car {
+class Czas {
 public:
-    Car() {name = "noname";power = -1;vin = -2;cout<<"constructor is running"<<endl;}
-    Car(const string &name, int vin) : name(name), vin(vin) {power = 7;cout<<"constructor is running"<<endl;}
-    void wypisz();
-    void zapisz(ostream& os) {os << "NAME: " << name << " POWER: " << power << " VIN: " << vin << endl;};
-    void wczytaj(istream& is) {is >> name >> power >> vin;};
+    Czas() {
+        h = 12;
+        m = 0;
+        s = 0;
+    }
 
-    virtual ~Car();
+    Czas(int hour, int minute, int second);
+
+    void wypisz();
+
+    int godzina();
+
+    void dodaj_sekunde();
+
+    bool porownaj(Czas czas);
+
+    virtual ~Czas();
+
+    static int getSuma();
+
+    void zapisz(ostream &os);
+
+    void wczytaj(istream &is);
 
 private:
-    string name;
-    int power;
-    int vin;
+    int h;
+    int m;
+    int s;
+    static int suma;
 };
 
-void Car::wypisz() {
-    cout<<"NAME: " << name << " POWER: " << power << " VIN: " << vin << endl;
+int Czas::suma = 0;
+
+void Czas::wczytaj(istream &is) {
+    string input;
+    is >> input;
+    h = (input[0] - 48) * 10 + input[1] - 48;
+    m = (input[3] - 48) * 10 + input[4] - 48;
+    s = (input[6] - 48) * 10 + input[7] - 48;
 }
 
-Car::~Car() {
-    cout << name << " destructor is running" << endl;
+void Czas::zapisz(ostream &os) {
+    if (h < 10)
+        os << "0";
+    os << h << ":";
+    if (m < 10)
+        os << "0";
+    os << m << ":";
+    if (s < 10)
+        os << "0";
+    os << s << endl;
+}
+
+bool Czas::porownaj(Czas czas) {
+    return h == czas.h && m == czas.m && s == czas.s;
+}
+
+void Czas::dodaj_sekunde() {
+    s++;
+    if (s == 60) {
+        s = 0;
+        m++;
+        if (m == 60) {
+            m = 0;
+            h++;
+            if (h == 24)
+                h = 0;
+        }
+    }
+}
+
+int Czas::godzina() {
+    return h;
+}
+
+Czas::~Czas() {
+    cout << "Destructing time ";
+    wypisz();
+}
+
+void Czas::wypisz() {
+    cout << h << ":" << m << ":" << s << endl;
+}
+
+Czas::Czas(int hour, int minute, int second = 0) {
+    if (hour >= 0 && hour < 24 && minute >= 0 && minute < 60 && second >= 0 && second < 60) {
+        h = hour;
+        m = minute;
+        s = second;
+    } else {
+        h = 12;
+        m = 0;
+        s = 0;
+    }
+    suma += s;
+}
+
+int Czas::getSuma() {
+    return suma;
 }
 
 int main() {
-    Car car1 = Car();
-    car1.wypisz();
-    Car car2 = Car("car_name",1542);
-    car2.wypisz();
+    Czas czas1;
+    Czas czas2(11, 59, 59);
+    czas1.wypisz();
+    czas2.wypisz();
 
-    ofstream outfile;
-    string filename = "wyniki.txt";
-    outfile.open (filename.c_str(), ios::out|ios::binary);
+    cout << czas2.godzina() << endl;
+
+    czas2.dodaj_sekunde();
+    czas2.wypisz();
+
+    cout << "Porownanie " << czas1.porownaj(czas2) << endl;
+
+    cout << "statis suma = " << Czas::getSuma() << endl;
 
     ifstream infile;
-    filename = "dane.txt";
-    infile.open (filename.c_str(), ios::in|ios::binary);
+    infile.open("dane.txt");
 
-    Car car3;
-    car3.wczytaj(infile);
-    car3.zapisz(outfile);
+    Czas czas3 = Czas(3, 2, 2);
+    czas3.wczytaj(infile);
 
+    Czas czas4;
+    czas4.wczytaj(infile);
+
+    infile.close();
+
+    ofstream outfile;
+    outfile.open("wyniki.txt");
+
+    czas1.zapisz(outfile);
+    czas2.zapisz(outfile);
+    czas3.zapisz(outfile);
+    czas4.zapisz(outfile);
+
+    outfile.close();
+
+    cout << "statis suma = " << Czas::getSuma() << endl;
 
     return 0;
 }
