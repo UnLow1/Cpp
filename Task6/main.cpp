48f594bf27947d41f876b3e4d36740e5a2d9e1d1
Adam Jamka <adam.jamka.1995@gmail.com>
2017-06-06 12:22:47 +0200

    Update main.cpp

--- Task6/main.cpp
+++ Task6/main.cpp
@@ -5,30 +5,37 @@
 #include <vector>
 #include <set>
 #include <map>
-#include "person.h"
+#include "article.h"
 
-Person RandomPerson() { return Person(); }
+int index = 10;
+
+int number;
+
+article RandomArticle() { return article(index++); }
+
+bool cmpNumber(article article) { return article.getNumber() == number; }
+
+bool myfunction(article a, article b) { return (a.getYear() < b.getYear()); }
 
 int main() {
     srand(time(NULL));
-    vector<Person> tab;
-
-    tab.push_back(Person());
-    tab.push_back(Person());
-    tab.push_back(Person());
-
-    std::set<Person> set;
-    std::set<Person>::iterator it;
-    std::map<string, Person> map;
-    std::map<string, Person>::iterator it_map;
-
-    vector<Person>::iterator it_tab = tab.begin();
+    vector<article> tab;
+    tab.push_back(article());
+    tab.push_back(article());
+    tab.push_back(article());
+    tab.push_back(article());
+    tab.push_back(article());
+    tab.push_back(article());
+    std::set<article> set;
+    std::set<article>::iterator it;
+    std::map<string, article> map;
+    std::map<string, article>::iterator it_map;
+    vector<article>::iterator it_tab = tab.begin();
     for (; it_tab != tab.end(); it_tab++) {
         cout << *it_tab << endl;
         set.insert(*it_tab);
         map[it_tab->get_key()] = *it_tab;
     }
-
     cout << endl << "TESTING SET" << endl;
     for (it = set.begin(); it != set.end(); ++it)
         cout << *it << endl;
@@ -38,20 +45,30 @@ int main() {
         cout << it_map->second << endl;
 
     cout << endl << "TESTING ALGORITHM LIBRARY" << endl;
-    std::vector<Person> myvector(20);
-    std::generate(myvector.begin(), myvector.end(), RandomPerson);
-
+    std::vector<article> myvector(30);
+    std::generate(myvector.begin(), myvector.end(), RandomArticle);
     std::cout << "myvector contains:\n";
-    std::vector<Person>::iterator it3;
-    for (it3 = myvector.begin(); it3 != myvector.end(); ++it3)
-        std::cout << *it3 << endl;
+    for (it_tab = myvector.begin(); it_tab != myvector.end(); ++it_tab)
+        std::cout << *it_tab << endl;
 
-    std::vector<Person>::iterator tmp;
-    it3 = find(tmp, myvector.end(), tmp->getBirthday() == 2000);
-    if (it3 != myvector.end())
+    std::vector<article>::iterator tmp = myvector.begin();
+    it_tab = find(tmp, myvector.end(), tmp->getYear() == 2000);
+    if (it_tab != myvector.end())
         std::cout << "Element found in myvector: " << *it << '\n';
     else
         std::cout << "Element not found in myvector\n";
+
+    int number, counter = 0;
+    cin >> number;
+    it_tab = myvector.begin();
+    counter = count_if(it_tab, myvector.end(), cmpNumber);
+    cout << "counter = " << counter << endl;
+
+    std::sort(myvector.begin(), myvector.end(), myfunction);
+
+    it_tab = std::unique(myvector.begin(), myvector.end());
+    myvector.resize(std::distance(myvector.begin(), it_tab));
+
 
     return 0;
 }
